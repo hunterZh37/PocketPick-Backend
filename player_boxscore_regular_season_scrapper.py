579e372ff36b1bb897550a2fe24b42c28e91b503
hunterZh37 <71850140+hunterZh37@users.noreply.github.com>
2024-05-26 19:53:50 -0700

    Update player_boxscore_regular_season_scrapper.py

--- player_boxscore_regular_season_scrapper.py
+++ player_boxscore_regular_season_scrapper.py
@@ -8,26 +8,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 
-
+# Setup Chrome options
 options = Options()
 options.headless = True
 driver_path = '/opt/homebrew/bin/chromedriver'
 s = Service(driver_path)
 driver = webdriver.Chrome(service=s, options=options)
 
-# Open the webpage
-driver.get("https://www.nba.com/stats/players/boxscores?SeasonType=Regular+Season")
+try:
+    # Open the webpage
+    driver.get("https://www.nba.com/stats/players/boxscores?SeasonType=Regular+Season")
+
+    # Increase wait time and verify correct selector
+    wait = WebDriverWait(driver, 20)
+    table = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table.Crom_table__p1iZz")))
+
+    # Verify if the table is found
+    if table:
+        print("Table found.", table)
+        headers = [header.text for header in table.find_elements(By.XPATH, ".//thead/tr/th")]
+        # date = day.find_element(By.CSS_SELECTOR, "h4.ScheduleDay_sdDay__3s2Xt").text
+        # games = day.find_elements(By.CSS_SELECTOR, "div.ScheduleGame_sg__RmD9I")
+        print("Headers:", headers)  # Debug print
+    else:
+        print("Table not found.")
+
+except Exception as e:
+    print("TimeoutException: Table not found within the specified wait time.", e)
+
+finally:
+    # Always close the driver
+    driver.quit()
 
 
-wait = WebDriverWait(driver, 10)
-schedule = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table.Crom_table__p1iZZ")))
 
-# Extract the table
-table = driver.find_element(By.CSS_SELECTOR, "table.Crom_table__p1iZZ")
-
-# Extract column headers
-headers = [header.text for header in table.find_elements(By.XPATH, ".//thead/tr/th")]
-print("Headers:", headers)  # Debug print
+# # Extract column headers
+# headers = [header.text for header in table.find_elements(By.XPATH, ".//thead/tr/th")]
+# print("Headers:", headers)  # Debug print
 
 # # Extract rows
 # rows = []
